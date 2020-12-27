c23bc2a51ab74163db65941d3ac6a2a615942b29
daviddennis02 <dennis.david02@gmail.com>
2020-12-27 06:14:25 +0100

    updated the app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Testing here, APP lives on EBS!'));
+app.get('/', (req, res) => res.send('We are testing here, APP lives on EBS!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
